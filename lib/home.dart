b00b4885a33afe58f6b67a70cee97327288beb74
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-09-17 12:26:16 +0300

    Create home.dart
    new page to add more structure

---
+++ lib/home.dart
@@ -0,0 +1,204 @@
+import 'dart:developer';
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:playground/utils.dart';
+import 'package:signature/signature.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  // initialize the signature controller
+  final SignatureController _controller = SignatureController(
+    penStrokeWidth: 4,
+    penColor: Colors.purple,
+    exportBackgroundColor: Colors.transparent,
+    exportPenColor: Colors.black,
+    onDrawStart: () => log('onDrawStart called!'),
+    onDrawEnd: () => log('onDrawEnd called!'),
+    strokeCap: StrokeCap.butt,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.addListener(() => log('Value changed'));
+  }
+
+  @override
+  void dispose() {
+    // IMPORTANT to dispose of the controller
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Future<void> exportImage(BuildContext context) async {
+    if (_controller.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          key: Key('snackbarPNG'),
+          content: Text('No content'),
+        ),
+      );
+      return;
+    }
+
+    final Uint8List? data =
+        await _controller.toPngBytes(height: 400, width: 400);
+    if (data == null) {
+      return;
+    }
+
+    if (!mounted) return;
+
+    await push(
+      context,
+      Scaffold(
+        appBar: AppBar(
+          title: const Text('PNG Image'),
+        ),
+        body: Center(
+          child: Container(
+            color: Colors.grey[300],
+            child: Image.memory(data),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> exportSVG(BuildContext context) async {
+    if (_controller.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          key: Key('snackbarSVG'),
+          content: Text('No content'),
+        ),
+      );
+      return;
+    }
+
+    final SvgPicture data = _controller.toSVG()!;
+
+    if (!mounted) return;
+
+    await push(
+      context,
+      Scaffold(
+        appBar: AppBar(
+          title: const Text('SVG Image'),
+        ),
+        body: Center(
+          child: Container(
+            color: Colors.grey[300],
+            child: data,
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Signature Application'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          const SizedBox(
+            height: 300,
+            child: Center(
+              child: Text('Big container to test scrolling issues'),
+            ),
+          ),
+          Signature(
+            key: const Key('signature'),
+            controller: _controller,
+            height: 300,
+            backgroundColor: Colors.grey[300]!,
+          ),
+          const SizedBox(
+            height: 300,
+            child: Center(
+              child: Text('Big container to test scrolling issues'),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          decoration: const BoxDecoration(color: Colors.black),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              IconButton(
+                key: const Key('exportPNG'),
+                icon: const Icon(Icons.image),
+                color: Colors.blue,
+                onPressed: () => exportImage(context),
+                tooltip: 'Export Image',
+              ),
+              IconButton(
+                key: const Key('exportSVG'),
+                icon: const Icon(Icons.share),
+                color: Colors.blue,
+                onPressed: () => exportSVG(context),
+                tooltip: 'Export Image',
+              ),
+              IconButton(
+                icon: const Icon(Icons.undo),
+                color: Colors.blue,
+                onPressed: () {
+                  setState(() {
+                    _controller.undo();
+                  });
+                },
+                tooltip: 'Undo',
+              ),
+              IconButton(
+                icon: const Icon(Icons.redo),
+                color: Colors.blue,
+                onPressed: () {
+                  setState(() {
+                    _controller.redo();
+                  });
+                },
+                tooltip: 'Redo',
+              ),
+              IconButton(
+                key: const Key('clear'),
+                icon: const Icon(Icons.clear),
+                color: Colors.blue,
+                onPressed: () {
+                  setState(() {
+                    _controller.clear();
+                  });
+                },
+                tooltip: 'Clear',
+              ),
+              IconButton(
+                key: const Key('stop'),
+                icon:
+                    Icon(_controller.disabled ? Icons.pause : Icons.play_arrow),
+                color: Colors.blue,
+                onPressed: () {
+                  setState(() {
+                    _controller.disabled = !_controller.disabled;
+                  });
+                },
+                tooltip: _controller.disabled ? 'Pause' : 'Play',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
