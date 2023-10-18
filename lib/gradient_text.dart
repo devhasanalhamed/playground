a31597b2ddf96a7299934370be61482449ae33a5
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-18 11:46:15 +0300

    Create gradient_text.dart
    example to make gradient text widget

---
+++ lib/gradient_text.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class GradientTextExample extends StatelessWidget {
+  const GradientTextExample({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ShaderMask(
+                shaderCallback: (bounds) => const RadialGradient(
+                  center: Alignment.topRight,
+                  radius: 1.0,
+                  colors: [
+                    Colors.orangeAccent,
+                    Colors.redAccent,
+                  ],
+                  tileMode: TileMode.mirror,
+                ).createShader(bounds),
+                child: const Text(
+                  'Text',
+                  style: TextStyle(
+                    fontSize: 56,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Text(
+                "Hello, Good Morning Everyone!",
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                  foreground: Paint()
+                    ..shader = const LinearGradient(
+                      colors: <Color>[
+                        Colors.pinkAccent,
+                        Colors.deepPurpleAccent,
+                        Colors.red
+                        //add more color here.
+                      ],
+                    ).createShader(
+                      const Rect.fromLTWH(0.0, 0.0, 200.0, 100.0),
+                    ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
