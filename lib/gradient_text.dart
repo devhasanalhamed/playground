f0e46b1a5c38417599e4d39f1361354f673351fb
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-18 11:46:49 +0300

    rename gradient text widget

--- lib/gradient_text.dart
+++ lib/gradient_text.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class GradientTextExample extends StatelessWidget {
-  const GradientTextExample({Key? key}) : super(key: key);
+class GradientText extends StatelessWidget {
+  const GradientText({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
