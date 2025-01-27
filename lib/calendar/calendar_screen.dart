a37810d66247b5f021abd4d6740c0df589f9ac1a
Hasan Al-Hamed <therealhassan97@gmail.com>
2025-01-27 09:53:29 +0300

    Update calendar_screen.dart

--- lib/calendar/calendar_screen.dart
+++ lib/calendar/calendar_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:playground/calendar/appointment_dialog.dart';
 import 'package:playground/calendar/calendar_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
@@ -8,30 +9,35 @@ class CalendarScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => CalendarProvider(),
-      child: Consumer<CalendarProvider>(
-        builder: (context, value, child) {
-          return Scaffold(
-            appBar: AppBar(
-              title: Text('Calendar'),
-            ),
-            body: SfCalendar(
-              allowedViews: value.views,
-              timeSlotViewSettings: value.timeSlotViewSettings,
-              dataSource: MeetingDataSource(value.getDataSource()),
-            ),
-            floatingActionButton: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                FloatingActionButton(
-                  onPressed: () {},
+    return Consumer<CalendarProvider>(
+      builder: (context, value, child) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('Calendar'),
+          ),
+          body: SfCalendar(
+            key: UniqueKey(),
+            allowedViews: value.views,
+            timeSlotViewSettings: value.timeSlotViewSettings,
+            dataSource: AppointmentsDataSource(value.appointments),
+          ),
+          floatingActionButton: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              FloatingActionButton(
+                onPressed: () => showModalBottomSheet(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  context: context,
+                  builder: (context) => AppointmentDialog(),
                 ),
-              ],
-            ),
-          );
-        },
-      ),
+                child: Icon(Icons.add),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
