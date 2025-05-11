fc4b79ddb8af7f25aae5d206eafea3cfca10a0cf
Hasan Al-Hamed <therealhassan97@gmail.com>
2025-05-11 11:27:06 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:playground/calendar/calendar_provider.dart';
-import 'package:playground/calendar/calendar_screen.dart';
-import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,13 +13,83 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
+const durations = [300, 600, 900, 1500, 2100]; // seconds
+
 class _MyAppState extends State<MyApp> {
+  late Timer timer;
+  late int t;
+  late int time;
+
+  start() {
+    timer = Timer.periodic(Duration(seconds: 1), (_) {
+      if (time <= 0) {
+        timer.cancel();
+      } else {
+        setState(() {
+          time -= 1;
+        });
+      }
+    });
+  }
+
+  reset() {
+    timer.cancel();
+    setState(() {
+      time = t;
+    });
+  }
+
+  @override
+  void initState() {
+    t = durations[0];
+    time = t;
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    timer.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => CalendarProvider(),
-      child: const MaterialApp(
-        home: CalendarScreen(),
+    final minutes = '${time ~/ 60}'.padLeft(2, '0');
+    final seconds = '${time % 60}'.padLeft(2, '0');
+    final currentTime = '$minutes:$seconds';
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Counter'),
+          centerTitle: true,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      currentTime,
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 16.0,
+              ),
+              if (time != t)
+                ElevatedButton(onPressed: reset, child: Text('Reset')),
+              if (time == t)
+                ElevatedButton(onPressed: start, child: Text('Start'))
+            ],
+          ),
+        ),
       ),
     );
   }
